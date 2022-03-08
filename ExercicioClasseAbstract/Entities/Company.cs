5528a22cb6db78dbb1c0c164aec7247829f175ea
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-03-08 10:40:55 -0300

    Exercicio de fixação para Classe Abstract

---
+++ ExercicioClasseAbstract/Entities/Company.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioClasseAbstract.Entities
+{
+    internal class Company : TaxPayer
+    {
+        public int NumberOfEmployers { get; set; }
+        public Company()
+        {
+        }
+        public Company(string name, double anualIncome, int numberOfEmployers) : base(name, anualIncome)
+        {
+            NumberOfEmployers = numberOfEmployers;
+        }
+        public override double Tax()
+        {
+            if (NumberOfEmployers <= 10)
+            {
+                return AnualIncome * 0.16;
+            }
+            else
+            {
+                return AnualIncome * 0.14;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Name}: $ {Tax().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExercicioClasseAbstract/Entities/Individual.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioClasseAbstract.Entities
+{
+    internal class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+        public Individual()
+        {
+        }
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            if (AnualIncome < 20000.00 && AnualIncome > 0.00)
+            {
+                double tax = AnualIncome * 0.15;
+                tax = tax - (HealthExpenditures * 0.50);
+                return tax;
+            }
+            else
+            {
+                double tax = AnualIncome * 0.25;
+                tax -= HealthExpenditures * 0.50;
+                return tax;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Name}: $ {Tax().ToString("F2",CultureInfo.InvariantCulture)}";
+        }
+    }
+}
